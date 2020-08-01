bf724949c1b6a5017a2ceaa781cac8b88f4fd012
Thiago Almeida <thiagoa.silva@outlook.com>
2020-08-01 18:44:00 -0300

    chore: added colors and font sizes constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  backgroundPrimary: '#212836',
+  backgroundSecondary: '#004f84',
+  buttonPrimary: '#01a8aa',
+  white: '#fff',
+  border: '#00fdc4',
+  borderSecondary: '#00fdc410',
+  transparent: '#00000000',
+};
---
+++ src/constants/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  extraLarge: 18,
+  large: 16,
+  regular: 14,
+  small: 12,
+  extraSmall: 10,
+};
