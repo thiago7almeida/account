0afd6d76386b88a52a8574947601fe31440b59a0
Thiago Almeida <thiagoa.silva@outlook.com>
2020-08-01 18:47:47 -0300

    feat: created Router component

---
+++ src/components/Router/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from '../../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
