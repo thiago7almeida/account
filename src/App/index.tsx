eded7272d4e5da6c30304b768064133c6fbe0215
Thiago Almeida <thiagoa.silva@outlook.com>
2020-08-02 19:36:43 -0300

    chore: configured message feedback

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {StatusBar, ActivityIndicator} from 'react-native';
+import {StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
+import FlashMessage from 'react-native-flash-message';
 
 import Router from '../components/Router';
+import store from '../store';
 
 const account: React.FC = () => {
   return (
-    <>
+    <Provider store={store}>
       <Router />
       <StatusBar translucent backgroundColor="transparent" />
-    </>
+      <FlashMessage position="center" />
+    </Provider>
   );
 };
 
